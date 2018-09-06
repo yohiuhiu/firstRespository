5b8687304e39405ba0fe2a8c89ee67c16517ea66
xiaokucha <ming3752656@163.com>
2018-09-06 13:31:06 +0800

    init

---
+++ src/test/java/com/example/demoweb/Main.java
@@ -0,0 +1,10 @@
+package com.example.demoweb;
+
+public class Main {
+    public static int sA;
+    public static void main(String[] args){
+            Main.sA=1;
+            int b;
+            int c;
+    }
+}
